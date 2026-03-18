b42248e19d9f3de744eec7398ccaf0a49697e2ba
Logan Gwin <logers520@gmail.com>
2026-03-18 17:13:29 -0500

    Added initial files

---
+++ bitMapDisplay.asm
@@ -0,0 +1,473 @@
+.data
+#Number fonts
+font_hole: .byte 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+font_0: .byte 0x3E, 0x61, 0x61, 0x61, 0x61, 0x61, 0x3E, 0x00
+font_1: .byte 0x18, 0x38, 0x18, 0x18, 0x18, 0x18, 0x7E, 0x00
+font_2: .byte 0x3E, 0x61, 0x01, 0x3E, 0x40, 0x40, 0x7F, 0x00
+font_3: .byte 0x3E, 0x01, 0x01, 0x3E, 0x01, 0x01, 0x3E, 0x00
+font_4: .byte 0x06, 0x0E, 0x16, 0x26, 0x7F, 0x06, 0x06, 0x00
+font_5: .byte 0x7F, 0x40, 0x40, 0x7E, 0x01, 0x01, 0x7E, 0x00
+font_6: .byte 0x3E, 0x40, 0x40, 0x7E, 0x61, 0x61, 0x3E, 0x00
+font_7: .byte 0x7F, 0x01, 0x02, 0x04, 0x08, 0x10, 0x10, 0x00
+font_8: .byte 0x3E, 0x61, 0x61, 0x3E, 0x61, 0x61, 0x3E, 0x00
+font_9: .byte 0x3E, 0x61, 0x61, 0x3F, 0x01, 0x01, 0x3E, 0x00
+font_10: .byte 0x00, 0x4E, 0x4A, 0x4A, 0x4A, 0x4A, 0x4E, 0x00
+
+.align 2
+font_table: 
+	.word font_0    # Index 0
+	.word font_1    # Index 1
+	.word font_2    # Index 2
+	.word font_3    # Index 3
+	.word font_4    # Index 4
+	.word font_5    # Index 5
+	.word font_6    # Index 6
+	.word font_7    # Index 7
+	.word font_8    # Index 8
+	.word font_9    # Index 9
+	.word font_10   # Index 10
+
+#Colors
+tile_background: .word 0x00909090 #Grey
+tile_border: .word 0x00464646 #Dark Grey
+game_background: .word 0x00FFFFFF #White
+
+#Dimensions
+game_dimensions: .word 512 #Board is 512 x 512
+pixel_dimensions: .word 64 #Pixel is 64 x 64
+
+#Test array
+arr1: .byte 36, 17, 34, 21, 19, 20, 18, 35, 0, 33, 26, 38, 37, 22, 39, 41, 23, 40, 42, 24, 25
+
+.text
+main:
+	#Fill board with white pixels
+	jal fill_board
+    	
+    	jal draw_board
+    	
+	#Render board from array
+	la $a0, arr1
+	li $a1, 21
+	jal update_board
+    	
+	# End program
+	li $v0, 10
+	syscall
+
+
+#Parameters: a0 = game array, a1 = element index
+#Return: v0 = player number, v1 = player value
+get_player_data:
+	#Get raw value from array
+	add $t0, $a0, $a1
+	lb $t1, 0($t0)		# $t1 = [PlayerID (MSB) | Number (LSB)]
+
+	#Extract player and number
+	move $v0, $t1		
+	srl $v0, $t1, 4 	#Shift first 4 bits right to get player number
+	andi $v1, $t1, 0x0F	#Mask lower 4 bits to get tile value
+	
+	#Retrn to caller
+	jr $ra
+	
+	
+	
+	
+#Parameters: a0 = game array, a1 = array size
+update_board:
+	# Store ra, s0-s7 to the stack
+	addi $sp, $sp, -36
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	sw $s4, 20($sp)
+	sw $s5, 24($sp)
+	sw $s6, 28($sp)
+	sw $s7, 32($sp)
+	
+	#s0 = game array, s1 = array size
+	move $s0, $a0
+	move $s1, $a1
+	
+	#s2 = current row, s3 = current column offset, s4 = max elements per row, s5 = total elements processed
+	li $s2, 2
+	li $s3, 3
+	li $s4, 1
+	li $s5, 0
+
+outer_render_loop:
+	li $s6, 0		#Row element counter (i = 0 to s4)
+	move $s7, $s3		#Temporary X to increment across the row
+
+inner_render_loop:
+	#Extract player data (v0 = player num, v1 = tile val)
+	move $a0, $s0
+	move $a1, $s5
+	jal get_player_data
+	
+	#Determine Color based on Player
+	li $a2, 0x00000000	# Default: Black (Player 2)
+	bne $v0, 1, color_set
+	li $a2, 0x00FF0000	# Set Red (Player 1)
+	
+	
+color_set:
+
+	#Extract player data (v0 = player num, v1 = tile val)
+	move $a0, $s0
+	move $a1, $s5
+	jal get_player_data
+	
+	#Draw number if valid (1-10) (v1 = num value)
+	beq $v1, $zero, skip_num
+	bge $v1, 11, skip_num
+
+	#Get font address from table [(num * 4) + font table address]
+	la $t0, font_table
+	sll $t3, $v1, 2	
+	add $t0, $t0, $t3
+	lw $a3, 0($t0)		#Get specific font address
+	
+	#Draw number on tile (x, y) (a2 already set)
+	move $a0, $s7
+	move $a1, $s2
+	jal draw_number
+
+skip_num:
+	addi $s7, $s7, 1		#Move to next tile column
+	addi $s5, $s5, 1		#Increment total elements processed
+	addi $s6, $s6, 1		#Increment row element counter
+	
+	# Loop if we haven't reached max elements for this row
+	blt $s6, $s4, inner_render_loop
+
+	#Update offsets for the next row
+	addi $s2, $s2, 1		#Row++
+	addi $s4, $s4, 1		#Elements per row++
+	
+	#If row is even, decrement column offset (y % 2 == 0)
+	andi $t0, $s2, 1
+	bne $t0, $zero, check_exit
+	addi $s3, $s3, -1		#Adjust pyramid center left
+
+check_exit:
+	#Exit if we processed all elements (21) or row count is too high [6 (row amount) + 2 (row offset) = 7 (final row)]
+	li $t0, 8
+	bge $s2, $t0, cleanup
+	blt $s5, $s1, outer_render_loop
+
+cleanup:
+	#Restore ra, s0-s7 from the stack
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	lw $s3, 16($sp)
+	lw $s4, 20($sp)
+	lw $s5, 24($sp)
+	lw $s6, 28($sp)
+	lw $s7, 32($sp)
+	addi $sp, $sp, 36
+	
+	#Return to caller
+	jr $ra
+	
+	
+
+		
+#Parameters: N/A
+draw_board:
+	# Store ra, s0-s7 to the stack
+	addi $sp, $sp, -36
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	sw $s4, 20($sp)
+	sw $s5, 24($sp)
+	sw $s6, 28($sp)
+	sw $s7, 32($sp)
+	
+	#Max tiles
+	li $s1, 21
+	
+	#s2 = current row, s3 = current column offset, s4 = max elements per row, s5 = total elements processed
+	li $s2, 2
+	li $s3, 3
+	li $s4, 1
+	li $s5, 0
+
+outer_draw_loop:
+	li $s6, 0		#Row element counter (i = 0 to s4)
+	move $s7, $s3		#Temporary X to increment across the row
+
+inner_draw_loop:
+
+	#Draw default tile (x, y)
+	move $a0, $s7
+	move $a1, $s2
+	jal draw_tile
+
+	addi $s7, $s7, 1		#Move to next tile column
+	addi $s5, $s5, 1		#Increment total elements processed
+	addi $s6, $s6, 1		#Increment row element counter
+	
+	# Loop if we haven't reached max elements for this row
+	blt $s6, $s4, inner_draw_loop
+
+	#Update offsets for the next row
+	addi $s2, $s2, 1		#Row++
+	addi $s4, $s4, 1		#Elements per row++
+	
+	#If row is even, decrement column offset (y % 2 == 0)
+	andi $t0, $s2, 1
+	bne $t0, $zero, check_exit1
+	addi $s3, $s3, -1		#Adjust pyramid center left
+
+check_exit1:
+	#Exit if we processed all elements (21) or row count is too high [6 (row amount) + 2 (row offset) = 7 (final row)]
+	li $t0, 8
+	bge $s2, $t0, cleanup1
+	blt $s5, $s1, outer_draw_loop
+
+cleanup1:
+	#Restore ra, s0-s7 from the stack
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	lw $s3, 16($sp)
+	lw $s4, 20($sp)
+	lw $s5, 24($sp)
+	lw $s6, 28($sp)
+	lw $s7, 32($sp)
+	addi $sp, $sp, 36
+	
+	#Return to caller
+	jr $ra
+	
+	
+	
+	
+#Fill board with white pixels
+fill_board:
+	#t1 = x
+	li $t1, 0
+	
+	#Address
+	addi $t3, $t3, 0x10040000
+	
+	#Get background color (Normal Grey)
+	la $t4, game_background
+	lw $t4, 0($t4)
+	
+	#Dimensions of screen (n x n)
+	li $t5, 512
+	
+	#Get num of iterations (t5 * t5)
+	mul $t5, $t5, $t5
+	
+	loop:
+		#Draw pixel at position
+		sw $t4, 0($t3)
+			
+		addi $t3, $t3, 4     # t3 to next pixel
+		addi $t1, $t1, 1     # x++
+        	blt $t1, $t5, loop # If x < iterations, keep drawing
+	
+	jr $ra
+
+
+
+
+#Parameters: $a0 = x (tile index), $a1 = y (tile index)
+draw_tile:
+	#Store s0 and s1 to stack
+	addi $sp, $sp, -8
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	
+	#Check what row the tile is on, if y is even, offset by 32 pixels
+	andi $t2, $a1, 1
+	
+	#Convert tile indices to pixel coordinates (64x64 chunks)
+	sll $a0, $a0, 6		#a0 = x * 64
+	sll $a1, $a1, 6		#a1 = y * 64
+    
+	#Calculate Start Address in $t1
+	li $t0, 512              # Screen Width
+	mul $t1, $a1, $t0        # y * 512
+	add $t1, $t1, $a0        # (y * 512) + x
+	sll $t1, $t1, 2          # Multiply by 4 for bytes
+	add $t1, $t1, 0x10040000 # Add Base Address
+	
+	#Add 128 offset to address if y is even (32 * 4 for bytes)
+	beq $t2, 1, skip_offset
+	addi $t1, $t1, 128
+	
+skip_offset:
+	#Pre-load colors into registers
+	la $t8, tile_border
+	lw $s0, 0($t8)           # $s0 = Border Color
+	la $t8, tile_background
+	lw $s1, 0($t8)           # $s1 = Background Color
+
+	#Setup Loop Counters
+	li $t2, 0                # Counter y = 0
+	li $t4, 1792             # Skip: (512 - 64) * 4
+
+outer_tile_loop:
+	li $t3, 0                # Counter x = 0
+
+inner_tile_loop:
+	#Determine color for this pixel
+	#Check Y-Border: if (y < 4 || y >= 60)
+	slti $t6, $t2, 4
+	sge  $t7, $t2, 60
+	or   $t6, $t6, $t7       #$t6 is 1 if y is on the border
+    
+	#Check X-Border: if (x < 4 || x >= 60)
+	slti $t8, $t3, 4
+	sge  $t9, $t3, 60
+	or   $t8, $t8, $t9       #$t8 is 1 if x is on the border
+    
+	#Combine: if (Y-Border OR X-Border)
+	or   $t6, $t6, $t8
+    
+	#Choose color
+	beq  $t6, $zero, paint_bg
+	move $t5, $s0            #Use Border color
+	j paint_pixel
+
+paint_bg:
+	move $t5, $s1            #Use Background color
+
+paint_pixel:
+	sw $t5, 0($t1)           #Draw to memory
+	addi $t1, $t1, 4         #Move pointer
+    
+	addi $t3, $t3, 1         #x++
+	blt $t3, 64, inner_tile_loop
+
+	#End of row: Add skip to jump to next screen line
+	add $t1, $t1, $t4        
+    
+	addi $t2, $t2, 1         #y++
+	blt $t2, 64, outer_tile_loop
+	
+	#Restore s0 and s1 from stack
+	lw $s0, 0($sp)
+	lw $s1, 4($sp)
+	addi $sp, $sp, 8
+	
+	#Return to caller
+	jr $ra
+	
+	
+
+		
+#Parameters: $a0 = x (tile index), $a1 = y (tile index), $a2 = color, $a3 = font_address
+draw_number:
+	# Save ra, s0, s1, and s2 on the stack
+	addi $sp, $sp, -16
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+
+	# Convert tile indices to pixel coordinates (64x64 chunks)
+	sll $s0, $a0, 6        # s0 = x_pixel
+	sll $s1, $a1, 6        # s1 = y_pixel
+    	
+    	# Store font color
+    	move $s2, $a2
+    	
+	# Check if the row is even (y % 2 == 0)
+	andi $t6, $a1, 1
+	beq  $t6, 1, start_loops
+    
+	addi $s0, $s0, 32      # If even, offset X by 32 pixels 
+
+start_loops:
+	li $t0, 0              # Row counter (0-7)
+    
+draw_row_loop:
+	lb $t1, 0($a3)          # Load font row byte
+	li $t2, 0               # Column counter (0-7)
+	li $t3, 0x80            # Mask
+
+draw_bit_loop:
+	# Calculate Address for the 4x4 block
+	sll $t7, $t0, 2         # row * 4
+	add $t7, $t7, $s1       # + y_pixel_start
+	addi $t7, $t7, 16       # + 16 centering
+    
+	sll $t8, $t2, 2         # col * 4
+	add $t8, $t8, $s0       # + x_pixel_start
+	addi $t8, $t8, 16       # + 16 centering
+
+	sll $t9, $t7, 9         # y * 512
+	add $t9, $t9, $t8       # + x
+	sll $t9, $t9, 2         # * 4
+	addi $t5, $t9, 0x10040000 # Base
+    	
+    	#Preload the tile background color if the mask doesn't line up
+    	la $t7, tile_background
+    	lw $a2, 0($t7)
+    	
+    	#Check if the bit in the mask and the font data line up
+	and $t4, $t1, $t3
+	beq $t4, $zero, default_color
+	
+	#Change color to font color if mask lines up
+	move $a2, $s2
+	
+default_color:
+	# Draw the chunk
+	jal draw_chunk
+
+	# Shift the mask over, increment the column
+	srl $t3, $t3, 1
+	addi $t2, $t2, 1
+	blt $t2, 8, draw_bit_loop
+	
+	# Increment row by 1 and move to the next byte in the font
+	addi $a3, $a3, 1
+	addi $t0, $t0, 1
+	blt $t0, 8, draw_row_loop
+    
+	# Restore and Return
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	addi $sp, $sp, 16
+	
+	#Return
+	jr $ra
+
+
+
+#Parameters: t5 = address to draw chunk, a2 = color of chunk
+draw_chunk:
+	li $t6, 0              # y_counter
+	move $t7, $t5          # current_address
+    
+outer_chunk_loop:
+	li $t8, 0              # x_counter
+inner_chunk_loop:
+	sw $a2, 0($t7)
+	addi $t7, $t7, 4       # Next pixel
+	addi $t8, $t8, 1       # x++
+	blt $t8, 4, inner_chunk_loop 
+    
+	addi $t7, $t7, 2032    # Skip to next row: (512*4) - (4*4)
+	addi $t6, $t6, 1       # y++
+	blt $t6, 4, outer_chunk_loop
+    	
+    	# Return to caller
+	jr $ra
---
+++ determineWinner.asm
@@ -0,0 +1,333 @@
+#Author: Logan Gwin
+#Date: 3/5/2026
+#Description: 
+#	This program will determine the winner of the game "Black Hole" by taking
+#	an array and summing up all the adjacent "tiles" next to the hole, then decoding
+#	them in the process to determine which player owns them and adding them to their
+#	running total.
+
+.include "SysCalls.asm"
+
+.data
+
+# First 4 LSB's hold the values 0-15, the 4 MSB's hold the player the value belongs to
+#Example: Tile that player 1 owns with a value of 9 = 0001 1001 or 9 + 16 = 25
+arr1: .byte 36, 17, 34, 21, 19, 20, 18, 35, 0, 33, 26, 38, 37, 22, 39, 41, 23, 40, 42, 24, 25
+arr2: .byte 0, 17, 33, 18, 0, 34, 19, 35, 20, 36
+
+player_1_win: .asciiz "Player 1 wins!\n"
+player_2_win: .asciiz "Player 2 wins!\n"
+
+input_message: .asciiz "Input Array:\n"
+score1_message: .asciiz "Player 1 score: "
+score2_message: .asciiz "Player 2 score: "
+
+space: .asciiz " "
+newline: .asciiz "\n"
+
+.text
+
+# Arguemnt a0 will be the array address
+la $a0, arr1
+# Arguement a1 will be the hole  position in the array
+#For arr1, pos = 8; For arr2, pos = 4
+li $a1, 8
+# Arguement a2 is the size of the array
+li $a2, 21
+
+determineWinner:
+	#Acquire s0-s5/ra and store them to stack
+	addi $sp, $sp, -28
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	sw $s2, 8($sp)
+	sw $s3, 12($sp)
+	sw $s5, 20($sp)
+	sw $ra, 24($sp)
+	
+	#Move the array address and hole position to s0 and s1 respectively, and store array size in s5
+	move $s0, $a0
+	move $s1, $a1
+	move $s5, $a2
+	
+	#Clear s2 and s3 to hold the running total of all their numbers close to the hole (s2 = player1, s3 = player2)
+	li $s2, 0
+	li $s3, 0
+	
+	#Determine what row the hole is on
+	#t0 is the running total of the sum of elements from each row
+	#t1 is the row counter and the amount that will be added to t0 each iteration
+	#We will loop until the sum of the elements is greater than our hole, then we will know what row the hole is in
+	li $t0, 0
+	li $t1, 1
+	
+	determineRow:
+	add $t0, $t0, $t1
+	slt $t2, $s1, $t0
+	bne $t2, $zero, Exit
+	addi $t1, $t1, 1
+	j determineRow
+	
+	#Exit when row is found
+	Exit:
+	
+	#Subtract 1 from t0 to convert it to the end index of the row
+	subi $t0, $t0, 1
+	
+	#If the hole is the top most element, jump to the appropriate calculation
+	beq $s1, 0, topHole
+	
+	#If the hole is on the right end of a row, we handle the calculation of the sum differently
+	beq $s1, $t0, rightEdgeHole
+	
+	#Get the left edge index of the row
+	sub $t2, $t0, $t1
+	addi $t2, $t2, 1
+	
+	#Check if the edge is on the left end of a row
+	beq $s1, $t2, leftEdgeHole
+	
+	#Otherwise, it is a normal center hole
+	j centerHole
+	
+	topHole:
+		#Decode n+1 tile
+		addi $t4, $s0, 1
+		jal decodeTile
+		
+		#Decode n+2 tile
+		addi $t4, $s0, 2
+		jal decodeTile
+		
+		#Jump to finish
+		j Finish
+	
+	rightEdgeHole:
+		#Copy array address to t4
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate the top tile n-(row)
+		sub $t4, $t4, $t1
+		jal decodeTile
+		
+		#Reset address and calculate the side hole n-1
+		move $t4, $s0
+		subi $t4, $t4, 1
+		jal decodeTile
+		
+		#Branch to finish if the hole pos == array size (no tiles beneath it)
+		bne $s1, $s5, Finish
+		
+		#Reset address
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate the tile at n+(row) and n+(row+1)
+		add $t4, $t4, $t1
+		jal decodeTile
+		
+		addi $t4, $t4, 1
+		jal decodeTile
+			
+		j Finish
+	
+	leftEdgeHole:
+		#Copy array address to t4
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate the top tile n-(row+1)
+		sub $t4, $t4, $t1
+		addi $t4, $t4, 1
+		jal decodeTile
+		
+		#Reset address and calculate the side hole n+1
+		move $t4, $s0
+		add $t4, $t4, $s1
+		addi $t4, $t4, 1
+		jal decodeTile
+		
+		#Branch to finish if the hole pos == beginning index of row (no tiles beneath it)
+		#Beginning index of row = n(n-1)/2 where n = row
+		subi $t5, $t5, 1
+		mul $t5, $t5, $t1
+		div $t5, $t5, 2
+		
+		bne $s1, $t5, Finish
+		
+		#Reset address
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate the tile at n+(row) and n+(row+1)
+		add $t4, $t4, $t1
+		jal decodeTile
+		
+		addi $t4, $t4, 1
+		jal decodeTile
+	
+		j Finish
+	
+	centerHole:
+		#Get address, offset it by the hole position
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate tile at n-(row) and n-(row+1)
+		sub $t4, $t4, $t1
+		jal decodeTile
+		addi $t4, $t4, 1
+		jal decodeTile
+		
+		#Reset address
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate tile at n-1 and n+1
+		subi $t4, $t4, 1
+		jal decodeTile
+		addi $t4, $t4, 2
+		jal decodeTile
+		
+		#Check if the hole is on the bottom of the board with n(n+1)/2 == array size, n = row
+		addi $t5, $t1, 1
+		mul $t5, $t5, $t1
+		div $t5, $t5, 2
+		beq $t5, $s5, Finish
+		
+		#Reset address
+		move $t4, $s0
+		add $t4, $t4, $s1
+		
+		#Calculate tiles at n+(row) and n+(row+1)
+		add $t4, $t4, $t1
+		jal decodeTile
+		addi $t4, $t4, 1
+		jal decodeTile
+		
+		j Finish
+	
+	decodeTile:
+		#Copy tile to t3
+		lb $t3, 0($t4)
+		
+		#Shift t3 right 4 times to extract the player number from the 4 MSB's
+		srl $t7, $t3, 4
+		
+		#Extract the tile value by masking the 4 LSB's
+		andi $t3, $t3, 0x0F
+		
+		#Check to see what player owns the tile, jump to respective player to add to total
+		beq $t7, 1, Player1
+		beq $t7, 2, Player2
+		
+		#Return to caller if player is unrecognized
+		jr $ra
+		
+		Player1:
+			#Add tile value to player 1's running total, return to caller
+			add $s2, $s2, $t3
+			jr $ra
+		Player2:
+			#Add tile value to player 2's running total, return to caller
+			add $s3, $s3, $t3
+			jr $ra
+
+	Finish:
+		#Print input message
+		li $v0, SysPrintString
+		la $a0, input_message
+		syscall
+		
+		#Copy array address
+		move $t4, $s0
+		move $t3, $zero
+		
+		printArray:
+			li $v0, SysPrintInt
+			#Get byte at pos, get tile value
+			lb $a0, 0($t4)
+			andi $a0, $a0, 0x0F
+			syscall
+			
+			li $v0, SysPrintString
+			la $a0, space
+			syscall
+			
+			#Increment the address and counter by 1
+			addi $t4, $t4, 1
+			addi $t3, $t3, 1
+			
+			#Check if the current iteration is greater than or equal to the size of the array
+			blt $t3, $s5, printArray
+		#Print newline
+		li $v0, SysPrintString
+		la $a0, newline
+		syscall
+		
+		#Print player 1 score msg
+		li $v0, SysPrintString
+		la $a0, score1_message
+		syscall
+		
+		#Print player 1 score
+		li $v0, SysPrintInt
+		move $a0, $s2
+		syscall
+		
+		#Print newline
+		li $v0, SysPrintString
+		la $a0, newline
+		syscall
+		
+		#Print player 2 score msg
+		li $v0, SysPrintString
+		la $a0, score2_message
+		syscall
+		
+		#Print player 2 score
+		li $v0, SysPrintInt
+		move $a0, $s3
+		syscall
+		
+		#Print newline
+		li $v0, SysPrintString
+		la $a0, newline
+		syscall
+		
+		#Compare scores
+		#If s3 > s2, Player 1 wins
+		slt $t5, $s2, $s3
+		bne $t5, $zero, P1Wins
+		
+		#If not, Player 2 wins
+		li $v0, SysPrintString
+		la $a0, player_2_win
+		syscall
+		#Load the player number into the function return register
+		li $v0, 2
+		j Cleanup
+
+	P1Wins:
+		#Print winner and load player number into function return register
+		li $v0, SysPrintString
+		la $a0, player_1_win
+		syscall
+		li $v0, 1
+
+	Cleanup:
+		#Restore s0-s5 and ra from the stack
+		lw $s0, 0($sp)
+    		lw $s1, 4($sp)
+    		lw $s2, 8($sp)
+    		lw $s3, 12($sp)
+    		lw $s5, 20($sp)
+    		lw $ra, 24($sp)
+    		addi $sp, $sp, 28
+    		#Jump to caller (commented out for Assigment 4 to prevent error)
+    		#jr $ra
+    		
+    		# Terminate program (Overwrites player won value, will not be here in term project)
+    		li $v0, SysExit
+    		syscall
